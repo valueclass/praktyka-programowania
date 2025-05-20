6c8b1927d98135614f5927467f16640bf0ce94e6
valueclass <112471480+valueclass@users.noreply.github.com>
2025-05-20 19:10:23 +0200

    Added basic CI/CD workflow
    * Added python-app workflow
    
    * Added documentation to `utils.py`
    
    * Reformatted `utils.py` and `test_utils.py`

--- utils.py
+++ utils.py
@@ -1,11 +1,47 @@
+"""
+Module that contains utility functions
+"""
+
+
 def add(a: int, b: int) -> int:
+    """
+    Perform addition on arguments and return the result.
+
+    :param int a: first argument
+    :param int b: second argument
+    :return: a + b
+    """
     return a + b
 
+
 def subtract(a: int, b: int) -> int:
+    """
+    Perform subtraction on arguments and return the result.
+
+    :param int a: first argument
+    :param int b: second argument
+    :return: a - b
+    """
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
+    """
+    Perform multiplication on arguments and return the result.
+
+    :param int a: first argument
+    :param int b: second argument
+    :return: a * b
+    """
     return a * b
 
+
 def divide(a: int, b: int) -> float:
+    """
+    Perform division on arguments and return the result.
+
+    :param int a: first argument
+    :param int b: second argument
+    :return: a * b
+    """
     return a / b
